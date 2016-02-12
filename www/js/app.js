303cc19e052651538328bee81e54690111c58eac
Mani <im.manikanta@gmail.com>
2016-02-12 14:45:12 +0530

    Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,102 @@
+var fb = new Firebase("https://photocollab.firebaseio.com/");
+
+angular.module('starter', ['ionic', 'ngCordova', 'firebase'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+          // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+          // for form inputs)
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+          // Don't remove this line unless you know what you are doing. It stops the viewport
+          // from snapping when text inputs are focused. Ionic handles this internally for
+          // a much nicer keyboard experience.
+          cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+          StatusBar.styleDefault();
+      }
+  });
+})
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+      .state("login", {
+          url: "/login",
+          templateUrl: "templates/login.html",
+          controller: "LoginController",
+          cache: false
+      })
+      .state("secure", {
+          url: "/secure",
+          templateUrl: "templates/secure.html",
+          controller: "SecureController"
+      });
+  $urlRouterProvider.otherwise('/login');
+})
+.controller("LoginController", function($scope, $state, $firebaseAuth) {
+
+    var fbAuth = $firebaseAuth(fb);
+
+    $scope.login = function(username, password) {
+        fbAuth.$authWithPassword({
+            email: username,
+            password: password
+        }).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }
+
+    $scope.register = function(username, password) {
+        fbAuth.$createUser({email: username, password: password}).then(function(userData) {
+            return fbAuth.$authWithPassword({
+                email: username,
+                password: password
+            });
+        }).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }
+
+})
+.controller("SecureController", function($scope, $ionicHistory, $firebaseArray, $cordovaCamera) {
+
+    $ionicHistory.clearHistory();
+
+    $scope.images = [];
+
+    var fbAuth = fb.getAuth();
+    if(fbAuth) {
+        var userReference = fb.child("users/" + fbAuth.uid);
+        var syncArray = $firebaseArray(userReference.child("images"));
+        $scope.images = syncArray;
+    } else {
+        $state.go("login");
+    }
+
+    $scope.upload = function() {
+        var options = {
+            quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+            targetWidth: 500,
+            targetHeight: 500,
+            saveToPhotoAlbum: false
+        };
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            syncArray.$add({image: imageData}).then(function() {
+                alert("Image has been uploaded");
+            });
+        }, function(error) {
+            console.error(error);
+        });
+    }
+
+});
